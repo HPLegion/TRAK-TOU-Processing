f36086ae87bde4a699b0fbbdc1651f3d12e54b3a
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-03-28 17:35:04 +0100

    setup for local installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tct',
+    version='0.1.0',
+    author='H. Pahl',
+    packages=['tct'],
+    description='Tools for Tricomp Simulation Suite',
+    long_description=open('README.md').read()
+)
