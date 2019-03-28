49baa94e24378f43c0707cd058f0cd8677504a1a
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-03-28 14:08:35 +0100

    import the required objects

--- tct/__init__.py
+++ tct/__init__.py
@@ -2,3 +2,7 @@
 tct is a collection of tools for working with input and output files of the tricomp
 simulation suite
 """
+from .import_tou import import_tou_as_beam, import_tou_as_particles
+from .import_min import import_min_as_regions
+from .beam_particle import Beam, Particle
+from .geometry import Region
